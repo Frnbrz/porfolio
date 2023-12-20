ae42f0314ec0ee4bdab6cc54ff51be7eea8df557
frnbrz <frnbrz@gmail.com>
2023-12-20 10:20:47 +0100

    Fix formatting in middleware/index.ts

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,9 +1,9 @@
-import { sequence } from "astro/middleware"
-import { useAstroI18n } from "astro-i18n"
+import { sequence } from "astro/middleware";
+import { useAstroI18n } from "astro-i18n";
 
 const astroI18n = useAstroI18n(
-	undefined /* config */,
-	undefined /* custom formatters */,
-)
+  undefined /* config */,
+  undefined /* custom formatters */,
+);
 
-export const onRequest = sequence(astroI18n)
+export const onRequest = sequence(astroI18n);
