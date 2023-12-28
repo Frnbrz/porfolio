feb28d7ce1d6934c3c2eb933afbcca3ec7fa25cb
frnbrz <frnbrz@gmail.com>
2023-12-28 17:07:53 +0100

    Add UI translations and language utilities

---
+++ src/i18n/ui.ts
@@ -0,0 +1,33 @@
+export const languages = {
+  en: 'English',
+  fr: 'Español',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'home.hi': "Hello World!",
+    'home.name': "I'm Francisco",
+    'home.description': "I'm a software developer {# alias #} based in Mexico City, I love to create and learn new things.",
+    'home.experience': "Experience",
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.experience': 'Experience',
+    'nav.project': 'Projects',
+    'nav.contact': 'Contact',
+
+  },
+  es: {
+    'home.hi': "Hola Mundo!",
+    'home.name': "Soy Francisco",
+    'home.description': "Soy un desarrollador de software {# alias #} con sede en la Ciudad de México, me encanta crear y aprender cosas nuevas.",
+    'home.experience': "Experiencia",
+    'nav.home': 'Inicio',
+    'nav.about': 'Sobre mi',
+    'nav.experience': 'Experiencia',
+    'nav.project': 'Proyectos',
+    'nav.contact': 'Contacto',
+  },
+} as const
+
