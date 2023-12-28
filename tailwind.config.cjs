a18f61c99c691da76c875cd147b14fda4bec1631
frnbrz <frnbrz@gmail.com>
2023-12-28 10:17:05 +0100

    Add Prettier configuration and update dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,astro}"],
-  darkMode: ['class', '[data-mode="dark"]'],
+  darkMode: "class",
   theme: {},
   plugins: [],
 };
