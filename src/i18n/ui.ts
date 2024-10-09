7b81caedb7d0927e56eef1cdaa39f7291823b740
frnbrz <frnbrz@gmail.com>
2024-10-09 11:26:05 +0200

    Added language

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,63 +1,86 @@
 export const languages = {
-  en: 'English',
-  fr: 'Español',
-}
+  en: "English",
+  fr: "Español",
+};
 
-export const defaultLang = 'en'
+export const defaultLang = "en";
 
 export const ui = {
   en: {
-    'home.hi': "Hello World!",
-    'home.name': "I'm Francisco",
-    'home.description': "Web developer ",
-    'home.experience': "Experience",
-    'home.projects': "Projects",
-    'home.daw': "CampusFP - Higher Technician in Web Application Development",
-    'home.daw_description': "Professional training that enables the design, development and implementation of web applications.",
-    'home.cloudappi': "CloudAppi - Frontend Developer",
-    'home.cloudappi_description': "Working with React, redux, material-ui, formik. Agile and scrum methodology.",
-    'badge.learn': 'Learn',
-    'badge.work': 'Work',
-    'nav.home': 'Home',
-    'nav.about': 'About',
-    'nav.experience': 'Experience',
-    'nav.project': 'Projects',
-    'nav.contact': 'Contact',
-    'project.duBo.title': 'DuBo Martial Arts',
-    'project.duBo.description': 'Web app for a martial arts school',
-    'project.calls.title': 'Calls App',
-    'project.calls.description': 'App to manage calls',
-    'project.blog.title': 'Blog',
-    'project.blog.description': 'Monorepo with api and web app',
-    'project.magicwater.title': 'MagicWater',
-    'project.magicwater.description': 'Web app for a water company to manage taks and clients',
-
+    "home.hi": "Hello World!",
+    "home.name": "I'm Francisco",
+    "home.description": "Web developer ",
+    "home.experience": "Experience",
+    "home.projects": "Projects",
+    "home.daw": "CampusFP - Higher Technician in Web Application Development",
+    "home.daw_description":
+      "Professional training that enables the design, development and implementation of web applications.",
+    "home.cloudappi": "CloudAppi - Frontend Developer",
+    "home.cloudappi_description":
+      "Working with React, redux, material-ui, formik. Agile and scrum methodology.",
+    "home.seidor": "Seidor - Desarrollador Frontend",
+    "home.seidor_description":
+      "Learning angular, rjsx, signals, standalone and angular material. Working with angular 14 ",
+    "badge.learn": "Learn",
+    "badge.work": "Work",
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.experience": "Experience",
+    "nav.project": "Projects",
+    "nav.contact": "Contact",
+    "project.apiquality.title": "API Tool integrator - API Quality",
+    "project.apiquality.description":
+      "Your API Tools Integrator for APIOps. Develop secure and quality APIs by implementing the API First methodology automatically",
+    "project.secura-insurance.title": "Secura Insurance",
+    "project.secura-insurance.description":
+      "Web app for an insurance company with responsive design",
+    "project.duBo.title": "DuBo Martial Arts",
+    "project.duBo.description": "Web app for a martial arts school",
+    "project.calls.title": "Calls App",
+    "project.calls.description": "App to manage calls",
+    "project.blog.title": "Blog",
+    "project.blog.description": "Monorepo with api and web app",
+    "project.magicwater.title": "MagicWater",
+    "project.magicwater.description":
+      "Web app for a water company to manage taks and clients",
   },
   es: {
-    'home.hi': "¡Hola Mundo!",
-    'home.name': "Soy Francisco",
-    'home.description': "Desarollador web",
-    'home.daw': "CampusFP- Técnico Superior en Desarrollo de Aplicaciones Web",
-    'home.daw_description': "Formación profesional que capacita a diseñar, desarrollar e implantar aplicaciones web.",
-    'home.cloudappi': "CloudAppi - Desarrollador Frontend",
-    'home.cloudappi_description': "Trabajando con React, redux, material-ui, formik. Metodologia agile y scrum.",
-    'home.experience': "Experiencia",
-    'home.projects': "Proyectos",
-    'badge.learn': 'Estudios',
-    'badge.work': 'Trabajo',
-    'nav.home': 'Inicio',
-    'nav.about': 'Sobre mi',
-    'nav.experience': 'Experiencia',
-    'nav.project': 'Proyectos',
-    'nav.contact': 'Contacto',
-    'project.duBo.title': 'DuBo Martial Arts',
-    'project.duBo.description': 'Aplicación web para una escuela de artes marciales',
-    'project.calls.title': 'Llamadas App',
-    'project.calls.description': 'App para gestionar llamadas',
-    'project.blog.title': 'Blog',
-    'project.blog.description': 'Monorepo con api y app web',
-    'project.magicwater.title': 'MagicWater',
-    'project.magicwater.description': 'Aplicación web para una empresa de agua para gestionar tareas y clientes',
+    "home.hi": "¡Hola Mundo!",
+    "home.name": "Soy Francisco",
+    "home.description": "Desarollador web",
+    "home.daw": "CampusFP- Técnico Superior en Desarrollo de Aplicaciones Web",
+    "home.daw_description":
+      "Formación profesional que capacita a diseñar, desarrollar e implantar aplicaciones web.",
+    "home.cloudappi": "CloudAppi - Desarrollador Frontend",
+    "home.cloudappi_description":
+      "Trabajando con React, redux, material-ui, formik. Metodologia agile y scrum.",
+    "home.seidor": "Seidor - Desarrollador Frontend",
+    "home.seidor_description":
+      "Aprendiendo angular, rjsx, signals, standalone y angular material. Trabajando con angular 14 ",
+    "home.experience": "Experiencia",
+    "home.projects": "Proyectos",
+    "badge.learn": "Estudios",
+    "badge.work": "Trabajo",
+    "nav.home": "Inicio",
+    "nav.about": "Sobre mi",
+    "nav.experience": "Experiencia",
+    "nav.project": "Proyectos",
+    "nav.contact": "Contacto",
+    "project.apiquality.title": "DuBo Martial Arts",
+    "project.apiquality.description":
+      " API Quality es una nueva herramienta que desarrolla APIs seguras y de calidad implementando la metodología API-First de manera automática",
+    "project.secura-insurance.title": "Secura Insurance",
+    "project.secura-insurance.description":
+      "Aplicación web para una empresa de seguros con diseño responsive",
+    "project.duBo.title": "DuBo Martial Arts",
+    "project.duBo.description":
+      "Aplicación web para una escuela de artes marciales",
+    "project.calls.title": "Llamadas App",
+    "project.calls.description": "App para gestionar llamadas",
+    "project.blog.title": "Blog",
+    "project.blog.description": "Monorepo con api y app web",
+    "project.magicwater.title": "MagicWater",
+    "project.magicwater.description":
+      "Aplicación web para una empresa de agua para gestionar tareas y clientes",
   },
-} as const
-
+} as const;
